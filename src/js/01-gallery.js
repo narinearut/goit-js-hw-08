bdbf9a76bf0d59a70d74498be2d4e13327b4578f
Narine <115227487+narinearut@users.noreply.github.com>
2023-04-04 23:44:39 +0100

    task 01

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,37 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
-// Change code below this line
+import SimpleLightbox from "simplelightbox";
+import '../css/common.css';
+import '../css/01-gallery.css';
 
-console.log(galleryItems);
+// Додатковий імпорт стилів
+import "simplelightbox/dist/simple-lightbox.min.css";
+import { galleryItems } from "./gallery-items.js";
+
+
+const galleryList = document.querySelector(".gallery");
+const itemsMarkup = createGalleryItems(galleryItems);
+
+galleryList.insertAdjacentHTML("beforeend", itemsMarkup);
+
+let lightbox = new SimpleLightbox(".gallery a", {
+  captionSelector: "img",
+  captionsData: "alt",
+  captionDelay: 250,
+});
+
+function createGalleryItems(items) {
+  return items
+    .map(
+      ({ preview, original, description }) =>
+        `<li class = "gallery__item">
+<a class = "gallery__link" href="${original}">
+  <img 
+  class = "gallery__image" 
+  src="${preview}" 
+  alt="${description}"
+  >
+
+</a>
+</li>`
+    )
+    .join("");
+};
